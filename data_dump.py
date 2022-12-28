eb6add14bfc904ef0f0c96b5c478575eebdc4d67
amiarnab123 <mannaarnab2001@gmail.com>
2022-12-28 21:17:21 +0530

    project deployment configured

--- data_dump.py
+++ data_dump.py
@@ -2,8 +2,7 @@ import pymongo
 import pandas as pd
 import json
 
-# Provide the mongodb localhost url to connect python to mongodb.
-client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
+from sensor.config import mongo_client
 
 DATABASE_NAME = "aps"
 COLLECTION_NAME = "sensor"
@@ -20,7 +19,7 @@ if __name__=="__main__":
     print(json_record[0])
 
     # insert converted record to mongodb
-    client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
+    mongo_client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
 
 
 
